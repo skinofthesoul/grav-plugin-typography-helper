fdd878d3292f8e16a43880f0bf4b01d9afde289c
Anna <ak@netzhexe.de>
2020-03-04 09:51:53 +0100

    initial commit

---
+++ typography-helper.php
@@ -0,0 +1,39 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+
+/**
+ * Class TypographyHelperPlugin
+ * @package Grav\Plugin
+ */
+class TypographyHelperPlugin extends Plugin
+{
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized()
+    {
+        // only proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+    }
+}
